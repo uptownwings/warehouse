20d193835631bcb694d12b5351489cd637f15bf7
Richard Beckker <richard@scoding.com>
2020-08-12 20:06:22 +0300

    Removed erroneous Vue usage of the auth libraries

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,73 +1,64 @@
-require('./bootstrap');
+require("./bootstrap");
 
-import 'es6-promise/auto'
-import axios from 'axios'
-import Vue from 'vue'
-import VueAuth from '@websanova/vue-auth'
-import VueAxios from 'vue-axios'
-import VueRouter from 'vue-router'
-import Index from './Index'
-import auth from './auth'
-import router from './router'
-import Chartkick from 'vue-chartkick'
-import Chart from 'chart.js'
-import VueConfirmDialog from 'vue-confirm-dialog'
-import VueInternationalization from 'vue-i18n'
-import messages from './locale/message';
+import "es6-promise/auto";
+import axios from "axios";
+import Vue from "vue";
+import VueAuth from "@websanova/vue-auth";
+import VueAxios from "vue-axios";
+import VueRouter from "vue-router";
+import Index from "./Index";
+import auth from "./auth";
+import router from "./router";
+import Chartkick from "vue-chartkick";
+import Chart from "chart.js";
+import VueConfirmDialog from "vue-confirm-dialog";
+import VueInternationalization from "vue-i18n";
+import messages from "./locale/message";
+
+// Set Vue globally
+window.Vue = Vue;
 
 // Set Vue router
-Vue.router = router
-Vue.use(VueRouter)
+Vue.router = router;
+Vue.use(VueRouter);
 
 // Internationalization
-Vue.use(VueInternationalization)
-const lang = localStorage.getItem('locale') || 'en';
+Vue.use(VueInternationalization);
+const lang = localStorage.getItem("locale") || "en";
 const i18n = new VueInternationalization({
     locale: lang,
     messages: messages
-})
+});
 
 // Initialize confirmation dialog
-Vue.use(VueConfirmDialog)
-Vue.component('vue-confirm-dialog', VueConfirmDialog.default)
+Vue.use(VueConfirmDialog);
+Vue.component("vue-confirm-dialog", VueConfirmDialog.default);
 
 // Initialize charting libraries
-Vue.use(Chartkick.use(Chart))
+Vue.use(Chartkick.use(Chart));
 
 // Define currency filter
-Vue.filter('toCurrency', function (value) {
-    value = parseFloat(value)
+Vue.filter("toCurrency", function(value) {
+    value = parseFloat(value);
 
-    var formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'EUR',
+    var formatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "EUR",
         minimumFractionDigits: 2
     });
     return formatter.format(value);
 });
 
 // Set Vue authentication
-Vue.use(VueAxios, axios)
-Vue.use(VueAuth, auth)
-axios.defaults.baseURL = `${process.env.MIX_APP_URL}/api/v1`
+Vue.use(VueAxios, axios);
+axios.defaults.baseURL = `${process.env.MIX_APP_URL}/api/v1`;
+Vue.use(VueAuth, auth);
 
-Vue.use(require('@websanova/vue-auth'), {
-    auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
-    http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
-    router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
- });
-
-
-// Set Vue globally
-window.Vue = Vue
-
-
-
-Vue.component('index', Index)
-Vue.component('pagination', require('laravel-vue-pagination'));
+Vue.component("index", Index);
+Vue.component("pagination", require("laravel-vue-pagination"));
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     i18n,
     router,
     messages
-})
+});
